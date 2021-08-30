707e13fb352f9d890c6e8e532a66a901c6fc39f2
Mirohaut <74530090+Mirohaut@users.noreply.github.com>
2021-08-30 13:57:26 +0300

    navi

---
+++ navigators/Navigator.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import Profile from '../views/Profile';
+import Home from '../views/Home';
+import Single from '../views/Single';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Tab = createBottomTabNavigator();
+
+const Stack = createNativeStackNavigator();
+
+const TabScreen = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
+  );
+};
+
+const StackScreen = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="FHome" component={TabScreen} />
+      <Stack.Screen name="Single" component={Single} />
+    </Stack.Navigator>
+  );
+};
+
+const Navigator = () => {
+  return (
+    <NavigationContainer>
+      <StackScreen />
+    </NavigationContainer>
+  );
+};
+
+export default Navigator;
